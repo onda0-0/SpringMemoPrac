4622dd3337a8f846d0d47d3a7b33cdab26b49336
onda0-0 <qwen0524@gmail.com>
2024-05-16 08:29:31 +0900

    init commit

---
+++ src/test/java/com/sparta/springmemo/SpringMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
