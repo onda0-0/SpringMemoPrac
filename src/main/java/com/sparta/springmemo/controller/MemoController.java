cf146ac62cadf1620351c56ee99dfcd6b8fe1b56
onda0-0 <qwen0524@gmail.com>
2024-05-16 10:07:34 +0900

    update, delete구현

--- src/main/java/com/sparta/springmemo/controller/MemoController.java
+++ src/main/java/com/sparta/springmemo/controller/MemoController.java
@@ -40,4 +40,29 @@ public class MemoController {
 
         return responseDtoList;
     }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        //해당메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            Memo memo=memoList.get(id);
+
+            //memo수정
+            memo.update(requestDto);
+            return memo.getId();
+        }else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
+    }
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id){
+        //해당메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            memoList.remove(id);
+            return id;
+        }else{
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
+
+    }
 }
